a0b9052b95890a05709c304e96e4c26171305b9a
thefron <thefron@wafflestudio.com>
2010-11-21 02:48:19 +0000

    admin authorization complete
    git-svn-id: svn+ssh://wafflestudio.net/home/thefron/svn/waffle@13 58158ecd-1a78-46b5-9736-0f4b7c5de568

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,40 @@
+require 'yaml'
+require 'sha1'
+
 class AdminController < ApplicationController
-  def index
+  before_filter :authorized?, :except => [:admin_index, :login]
+
+  def admin_index
+    unless session[:admin].nil?
+      redirect_to admin_members_path
+    end
+  end
+
+  def login
+    if check_credential(params[:id], params[:password])
+      session[:admin] = params[:id]
+      redirect_to admin_members_path
+    else
+      flash[:error] = 'id or password is wrong'
+      redirect_to admin_path
+    end
+  end
+
+  def logout
+    session[:admin] = nil
+    redirect_to '/'
+  end
+
+  private
+
+  def authorized?
+    if !session[:admin]
+      redirect_to admin_path
+    end
+  end
+
+  def check_credential(id, password)
+    credentials = YAML::load_file("#{RAILS_ROOT}/config/admin.yml")
+    !credentials.find{|c| c[:id] == id and c[:password] == SHA1::hexdigest(password)}.nil?
   end
 end
--- new_admin.rb
+++ new_admin.rb
@@ -3,31 +3,34 @@ require 'sha1'
 
 #system "touch #{RAILS_ROOT}/config/admin.yml"
 #admin_credentials = YAML::load_file("#{RAILS_ROOT}/config/admin.yml")||[]
-system "touch config/admin.yml"
-admin_credentials = YAML::load_file("config/admin.yml")||[]
-while true do
-  print 'New ID: '
-  new_id = gets
-  if admin_credentials.find{|admin| admin[:id] == new_id.strip}
-    puts "Already existing ID. Try another"
-    next
-  end
-  system "stty -echo"
-  print 'Password: '
-  new_password = gets
-  puts ' '
-  print 'Password again: '
-  confirm_password = gets
-  puts ' '
-  system "stty echo"
-  if confirm_password == new_password
-    admin_credentials.push({:id => new_id.strip, :password => SHA1::hexdigest(new_password.strip)})
+begin
+  system "touch config/admin.yml"
+  admin_credentials = YAML::load_file("config/admin.yml")||[]
+  while true do
+    print 'New ID: '
+    new_id = gets
+    if admin_credentials.find{|admin| admin[:id] == new_id.strip}
+      puts "Already existing ID. Try another"
+      next
+    end
+    system "stty -echo"
+    print 'Password: '
+    new_password = gets
+    puts ' '
+    print 'Password again: '
+    confirm_password = gets
+    puts ' '
+    system "stty echo"
+    if confirm_password == new_password
+      admin_credentials.push({:id => new_id.strip, :password => SHA1::hexdigest(new_password.strip)})
 #    yaml = File.open("#{RAILS_ROOT}/config/admin.yml", "w")
-    yaml = File.open("config/admin.yml", "w")
-    yaml.write(YAML::dump(admin_credentials))
-    yaml.close
-    puts "Successfully created admin!!"
-    break
+      yaml = File.open("config/admin.yml", "w")
+      yaml.write(YAML::dump(admin_credentials))
+      yaml.close
+      puts "Successfully created admin!!"
+      break
+    end
   end
+rescue NoMethodError, Interrupt
+  system "stty echo"
 end
-
