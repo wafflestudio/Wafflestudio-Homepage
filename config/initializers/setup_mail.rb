fd081e7a529731ac0df64d6e0808a140cdf9aa5d
thefron <thefron@wafflestudio.com>
2010-11-30 06:32:30 +0000

    uses sendmail
    git-svn-id: svn+ssh://wafflestudio.net/home/thefron/svn/waffle@27 58158ecd-1a78-46b5-9736-0f4b7c5de568

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,4 +1,5 @@
 #ActionMailer::Base.raise_delivery_errors = true
+=begin
 ActionMailer::Base.perform_deliveries = true
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
@@ -10,3 +11,9 @@ ActionMailer::Base.smtp_settings = {
   :authentication => "plain",
   :enable_starttls_auto => true
 }
+=end
+ActionMailer::Base.delivery_method = :sendmail
+ActionMailer::Base.sendmail_settings = {
+  :location       => '/usr/sbin/sendmail',
+  :arguments      => '-i -t'
+} 
