7729d7df0e5850293200f3fdf7c98ca544aeb7e9
thefron <thefron@wafflestudio.com>
2010-12-01 10:54:37 +0000

    minor fix
    git-svn-id: svn+ssh://wafflestudio.net/home/thefron/svn/waffle@29 58158ecd-1a78-46b5-9736-0f4b7c5de568

--- app/mailers/waffle_mailer.rb
+++ app/mailers/waffle_mailer.rb
@@ -1,8 +1,7 @@
 class WaffleMailer < ActionMailer::Base
-  default :from => "hello@wafflestudio.com", :to => "hello@wafflestudio.com"
 
   def contact_to_waffle(contact)
     @contact = contact
-    mail(:subject => "[문의]#{contact.message.mb_chars[0..30].to_s}")
+    mail(:from => "#{contact.name} <#{contact.email}>", :to => "hello@wafflestudio.com", :subject => "[문의]#{contact.message.mb_chars[0..30].to_s}")
   end
 end
--- send_mail.rb
+++ send_mail.rb
@@ -1,8 +1,8 @@
-puts 'mail sender loaded'
+puts "[#{Time.now.strftime("%Y-%m-%d %H:%M:%S")}] mail sender loaded"
 unsent_contact = Contact.where(:mail_status => 'unsent')
-puts 'no contacts to send' if unsent_contact.length < 1
+puts "[#{Time.now.strftime("%Y-%m-%d %H:%M:%S")}] no contacts to send" if unsent_contact.length < 1
 unsent_contact.each do |contact|
   WaffleMailer.contact_to_waffle(contact).deliver
   contact.update_attributes({:mail_status => 'sent'})
-  puts "Contact(#{contact.id}) sent!"
+  puts "[#{Time.now.strftime("%Y-%m-%d %H:%M:%S")}] Contact(#{contact.id}) sent!"
 end
