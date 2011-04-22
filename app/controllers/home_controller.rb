ccfad5bf4e77c173d1b30f228823f5f24ed12dae
thefron <thefron@wafflestudio.com>
2011-04-22 04:53:37 +0000

    show carousels only visible
    git-svn-id: svn+ssh://wafflestudio.net/home/thefron/svn/waffle@72 58158ecd-1a78-46b5-9736-0f4b7c5de568

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @carousels = Carousel.all.reverse
+    @carousels = Carousel.where(:visibility => 'visible').reverse
     @timelines = Timeline.all
     @members = Member.all
     @projects = Project.all
