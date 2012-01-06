4f48ee7e3ae6aed93b41a1eaafb94e94873a82aa
Hoseong Hwang <thefron@wafflestudio.com>
2012-01-06 14:18:58 +0900

    Add admin[css|js] into precompile list

--- config/application.rb
+++ config/application.rb
@@ -45,5 +45,7 @@ module Waffle
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.assets.precompile += ['admin.js', 'admin.css']
   end
 end
