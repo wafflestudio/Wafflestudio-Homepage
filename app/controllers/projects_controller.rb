7df599cbdb60b468c5bbbb5fe12c40fe3be568bd
Leeingnyo <dyd1928@naver.com>
2016-01-21 15:04:42 +0900

    fix typo

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,6 @@
 class ProjectsController < ApplicationController
   def show
-    project = Project.find params[:id].as_json(:include => {:members => {:methods => :list1_url_thumb}, :screenshots => {:methods => [:image_big, :image_thumb]}, :logo => {:methods => :image_logo}})
+    project = Project.find(params[:id]).as_json(:include => {:members => {:methods => :list1_url_thumb}, :screenshots => {:methods => [:image_big, :image_thumb]}, :logo => {:methods => :image_logo}})
     project["project"]["thumbnail_urls"] = project["project"]["screenshots"].map{|s| s["image_thumb"]}[0..2]
     project["project"]["screenshot_urls"] = project["project"]["screenshots"].map{|s| s["image_big"]}[0..2]
     project["project"]["logo_url"] = project["project"]["logo"].nil? ? nil : project["project"]["logo"]["image_logo"]
