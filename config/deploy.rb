b7adf38090c795f36395d6b3f6539ca66c238b24
Leeingnyo <dyd1928@naver.com>
2016-08-29 01:26:11 +0900

    erase regacy and initialize capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,133 +1,36 @@
-$:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
-require "rvm/capistrano"                  # Load RVM's capistrano plugin.
-set :rvm_ruby_string, '1.8.7'
+# config valid only for current version of Capistrano
+lock '3.6.1'
 
-set :using_rvm, true
-set :rvm_type, :system
-set :application, "wafflestudio_homepage"
-set :repository,  "git@github.com:waffle-studio/Wafflestudio-Homepage.git"
+set :application, 'my_app_name'
+set :repo_url, 'git@example.com:me/my_repo.git'
 
-set :scm, :git
-set :branch, "master"
-set :ssh_options, { :forward_agent => true }
-set :deploy_to, "/var/www/wafflestudio_homepage"
-set :assets_prefix, 'assets'
-set :asset_env, "RAILS_GROUPS=assets"
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
-role :web, "wafflestudio.com"
+# Default deploy_to directory is /var/www/my_app_name
+# set :deploy_to, '/var/www/my_app_name'
 
-set :user, 'wafflehomepage'
-default_run_options[:pty] = true
+# Default value for :scm is :git
+# set :scm, :git
 
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
 
-# If you are using Passenger mod_rails uncomment this:
-namespace :deploy do
-  task :default, :roles => :web do
-    update
-    restart
-  end
-  task :update, :roles => :web do
-    transaction do
-      update_code
-      symlink
-      symlink_shared
-    end
-  end
-  task :bundle, :roles => :web do
-    run "cd #{current_path} && bundle install"
-  end
-  task :update_code, :roles => :web, :except => { :no_release => true } do
-    on_rollback { run "rm -rf #{release_path}; true" }
-    strategy.deploy!
-    finalize_update
-  end
-  task :start do ; end
-  task :stop do ; end
-  task :restart, :roles => :web, :except => { :no_release => true } do
-    run "#{try_sudo} touch #{current_path}/tmp/restart.txt"
-  end
-  task :symlink, :roles => :web, :except => { :no_release => true } do
-    on_rollback do
-      if previous_release
-        run "rm -f #{current_path}; ln -s #{previous_release} #{current_path}; true"
-      else
-        logger.important "no previous release to rollback to, rollback of symlink skipped"
-      end
-    end
+# Default value for :pty is false
+# set :pty, true
 
-    run "rm -f #{current_path} && ln -s #{latest_release} #{current_path}"
-  end
-    task :setup_shared, :roles => :web do
-    sudo "chown -R wafflehomepage:wafflehomepage #{shared_path}/.."
-    run "mkdir #{shared_path}/db"
+# Default value for :linked_files is []
+# append :linked_files, 'config/database.yml', 'config/secrets.yml'
 
-    # SYMLINK stalk
+# Default value for linked_dirs is []
+# append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
 
-    puts "Now edit the config files in #{shared_path}."
-  end
-  task :symlink_shared, :roles => :web do
-    run "ln -nfs #{shared_path}/db/production.sqlite3 #{latest_release}/db/production.sqlite3"
-    run "ln -nfs #{shared_path}/config/admin.yml #{latest_release}/config/admin.yml"
-  end
-  task :finalize_update, :roles => :web, :except => { :no_release => true } do
-    run "chmod -R g+w #{latest_release}" if fetch(:group_writable, true)
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
-    # mkdir -p is making sure that the directories are there for some SCM's that don't
-    # save empty folders
-    run <<-CMD
-      rm -rf #{latest_release}/log #{latest_release}/tmp/pids &&
-      mkdir -p #{latest_release}/tmp &&
-      ln -s #{shared_path}/log #{latest_release}/log &&
-      ln -s #{shared_path}/pids #{latest_release}/tmp/pids &&
-      ln -s #{shared_path}/system #{latest_release}/public/system
-    CMD
-
-    if fetch(:normalize_asset_timestamps, true)
-      stamp = Time.now.utc.strftime("%Y%m%d%H%M.%S")
-      asset_paths = %w(images stylesheets javascripts).map { |p| "#{latest_release}/public/#{p}" }.join(" ")
-      run "find #{asset_paths} -exec touch -t #{stamp} {} ';'; true", :env => { "TZ" => "UTC" }
-    end
-  end
-  task :cleanup, :roles => :web, :except => { :no_release => true } do
-    count = fetch(:keep_releases, 5).to_i
-    if count >= releases.length
-      logger.important "no old releases to clean up"
-    else
-      logger.info "keeping #{count} of #{releases.length} deployed releases"
-
-      directories = (releases - releases.last(count)).map { |release|
-        File.join(releases_path, release) }.join(" ")
-
-        try_sudo "rm -rf #{directories}"
-    end
-  end
-  namespace :assets do
-    # Refer comments regarding these tasks from capistrano-2.9.0 gem
-    task :symlink, :roles => :web, :except => { :no_release => true } do
-      run <<-CMD
-        rm -rf #{latest_release}/public/#{assets_prefix} &&
-        mkdir -p #{latest_release}/public &&
-        mkdir -p #{shared_path}/assets &&
-        ln -s #{shared_path}/assets #{latest_release}/public/#{assets_prefix}
-        CMD
-    end
-
-    task :precompile, :roles => :web, :except => { :no_release => true } do
-      run "cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile"
-    end
-
-    task :clean, :roles => :web, :except => { :no_release => true } do
-      run "cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:clean"
-    end
-  end
-end
-
-after  "deploy",             "deploy:cleanup" # keeps only last 5 releases
-after  "deploy:setup",       "deploy:setup_shared"
-before 'deploy:finalize_update', 'deploy:assets:symlink'
-after "deploy:symlink_shared", "deploy:bundle"
-after 'deploy:symlink_shared', 'deploy:assets:precompile'
-
+# Default value for keep_releases is 5
+# set :keep_releases, 5
